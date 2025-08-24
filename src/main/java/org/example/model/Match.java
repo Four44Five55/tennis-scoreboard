99743ca4f53165f4e935594cbd55d5a96f20ac1b
Four44Five55 <Four4Five55@yandex.ru>
2025-08-24 15:11:24 +0300

    feat(model): Добавить сущности player, mathc

---
+++ src/main/java/org/example/model/Match.java
@@ -0,0 +1,57 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "matches")
+public class Match {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "player1", nullable = false)
+    private Player player1;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "player2", nullable = false)
+    private Player player2;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "winner")
+    private Player winner;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(Player player1) {
+        this.player1 = player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(Player player2) {
+        this.player2 = player2;
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
+
+    public void setWinner(Player winner) {
+        this.winner = winner;
+    }
+
+}
